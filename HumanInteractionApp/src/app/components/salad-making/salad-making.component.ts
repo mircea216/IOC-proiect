3a6425591f84b02d9a7075d056ca7cc1008ae58a
mircea216 <75624674+mircea216@users.noreply.github.com>
2023-01-14 15:20:03 +0200

    retry sound

--- HumanInteractionApp/src/app/components/salad-making/salad-making.component.ts
+++ HumanInteractionApp/src/app/components/salad-making/salad-making.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./salad-making.component.scss']
 })
 export class SaladMakingComponent implements OnInit {
+  private audioRetry: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  playRetrySound() {
+    this.audioRetry = new Audio();
+    this.audioRetry.src = "../../../assets/sound/retry-sound.m4a"
+    this.audioRetry.load();
+    this.audioRetry.play();
+  }
 }
